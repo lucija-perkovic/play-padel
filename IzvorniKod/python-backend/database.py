46e5862c93bbfa59380643c56d29b26c27525039
mk50905 <mk50905@fer.hr>
2024-10-30 14:16:53 +0100

    create python backend skeleton

---
+++ IzvorniKod/python-backend/database.py
@@ -0,0 +1,17 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from pymongo.collection import Collection
+
+MONGO_URI = "mongodb+srv://<username>:<password>@play-padel-cluster.bq1gt.mongodb.net/?retryWrites=true&w=majority&appName=play-padel-cluster"
+MONGO_DB = "play-padel-database-1"
+MONGO_COLLECTION = "play-padel-collection-1"
+
+client = MongoClient(MONGO_URI, server_api=ServerApi('1'))
+database = client[MONGO_DB]
+collection: Collection = database[MONGO_COLLECTION]
+
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)

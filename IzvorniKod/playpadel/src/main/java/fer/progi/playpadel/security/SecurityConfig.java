aa8817c052ac336131caaf6307b9ce2a2dd19c60
LukaOkovic <luka.okovic@fer.hr>
2025-01-22 21:59:08 +0100

    Migrate H2 to Postgres

--- IzvorniKod/playpadel/src/main/java/fer/progi/playpadel/security/SecurityConfig.java
+++ IzvorniKod/playpadel/src/main/java/fer/progi/playpadel/security/SecurityConfig.java
@@ -6,13 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import java.util.Optional;
 
 @Configuration
 public class SecurityConfig {
@@ -36,9 +33,11 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+        http.csrf(csrf -> csrf
+                        .ignoringRequestMatchers("/h2-console/**", "/actuator/**")
+                )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/user/login", "/user/register").permitAll()
+                        .requestMatchers("/user/login", "/user/register", "/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
